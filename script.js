ff7286f0f0d46266a387f68099e0221dab33a800
Ezeh fidelis <Kinglegacy965@gmail.com>
2024-09-19 09:40:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const userLanguage = navigator.userLanguage;
-const defaultLanguage = 'en';
-const languageSwitcher = document.getElementById('language-switcher');
-languageSwitcher.value = userLanguage;
-languageSwitcher.addEventListener('change', (e) =>{
-    const selectedLanguage = e.target.value;
-    updateContent(selectedLanguage);
-})
-function updateContent(selectedLanguage) {
-    const translatableElements = document.querySelectorAll('[data-element]');
-translatableElements.forEach((element) => {
-    const translation = element.getAttribute(data-translate-$ {language});
-    element.textContent = translation;
-});
-}
+function toggleAside() {
+    var aside = document.getElementById("aside");
+    if (aside.style.display === "none") {
+      aside.style.display = "block";
+    } else {
+      aside.style.display = "none";
+    }
+  }
+  
+  
